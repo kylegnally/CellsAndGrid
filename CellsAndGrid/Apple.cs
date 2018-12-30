2604cefc7b494519debd57b5a96602f97c3d7867
Kyle G. Nally <kylegnally@gmail.com>
2018-12-30 18:39:51 -0500

    Add IGenericQueue interface. Add GenericQueue class. Add Apple class.

---
+++ CellsAndGrid/Apple.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellsAndGrid
+{
+    class Apple
+    {
+        private string _apple = "o";
+        private int _nutrition = 1;
+
+        public Apple()
+        {
+            AnApple = _apple;
+            Nutrition = _nutrition;
+        }
+
+        public string AnApple
+        {
+            get => _apple;
+            set => _apple = value;
+        }
+
+        public int Nutrition
+        {
+            get => _nutrition;
+            private set => _nutrition = value;
+        }
+    }
+}
