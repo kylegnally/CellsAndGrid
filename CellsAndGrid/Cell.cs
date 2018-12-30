f8e8d71aa1d61071682eef535adf126962b4c577
Kyle G. Nally <kylegnally@gmail.com>
2018-12-30 11:24:05 -0500

    Add code to Cell class to test whether a cell is sitting on the edge of the map.
    TODO: disallow player from moving into edge spaces.

--- CellsAndGrid/Cell.cs
+++ CellsAndGrid/Cell.cs
@@ -10,12 +10,14 @@ namespace CellsAndGrid
             YPosition = y;
             Selected = false;
             Visited = false;
+            TestForEdge(x, y, gridSize);
         }
 
         public int XPosition { get; set; }
         public int YPosition { get; set; }
         public bool Selected { get; set; }
         public bool Visited { get; set; }
+        public bool EdgeCell { get; set; }
         public string Contents { get; private set; }
         
         public void DetermineContents(int x, int y, int grid)
@@ -27,12 +29,22 @@ namespace CellsAndGrid
             }
             else
             {
+                
                 if (Visited)
                 {
                     Contents = "  ";
                 }
+                else if (EdgeCell) Contents = "% ";
                 else Contents = "- ";
             }
+        }
+
+        public void TestForEdge(int x, int y, int size)
+        {
+            if (y == 0) EdgeCell = true;
+            if (x == 0) EdgeCell = true;
+            if (x == size - 1) EdgeCell = true;
+            if (y == size - 1) EdgeCell = true;
         }
     }
 }
