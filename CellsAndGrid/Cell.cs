4f056619d9813aa6f2ee6da1f0b3571f2f83834d
Kyle G. Nally <kylegnally@gmail.com>
2018-12-30 18:58:08 -0500

    Add a note in the Cell class describing what to do to make the snake game work.

--- CellsAndGrid/Cell.cs
+++ CellsAndGrid/Cell.cs
@@ -19,6 +19,13 @@ namespace CellsAndGrid
         public bool Visited { get; set; }
         public bool EdgeCell { get; set; }
         public string Contents { get; private set; }
+
+        // Somewhere in this class you will want to write code that will implement the GenericQueue 
+        // class to create a linked list of segments that will allow each segment to "follow" the
+        // one before it. The data in each node will be a two-dimensional array containing the
+        // coordinates for each segment and these coordinates will update with each press of the arrow 
+        // key. On redraw, this will give the appearance that the snake is moving along the path
+        // defined by the user.
         
         public void DetermineContents(int x, int y, int grid)
         {
