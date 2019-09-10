9c584aeb35ce6009ba4c2a089a9d60403d793233
Kyle G. Nally <kylegnally@hotmail.com>
2019-09-09 20:08:34 -0400

    Create two classes.

---
+++ CellsAndGrid/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellsAndGrid
+{
+    class GameManager
+    {
+        private int _x;
+        private int _y;
+        private int _size;
+
+        private Grid _grid;
+        private Player _player;
+        private Cell _oneCell;
+
+        public Grid Playfield { get; set; }
+
+    }
+}
